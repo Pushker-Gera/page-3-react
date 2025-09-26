f4c9cd577c7ca32eca161ff787a448629f27c8ae
pushker gera <pushkergera@pushkers-MacBook-Air.local>
2025-09-26 11:22:37 +0530

    rename: icon component

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Icon_box from './icon_box'
+import IconsBox from './IconsBox'
 
 const UserCard = (props) => {
   return (
     <div className="card">
       <div className="relative">
         <img className="w-100" src={props.img} alt={props.img_name} />
-        <Icon_box />
+        <IconsBox />
         <h2 className="name">{props.name}</h2>
         <p className="designation">{props.designation}</p>
       </div>
