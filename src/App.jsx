6c016711700e8b6e3ddc26ad423f8481fdaeaffb
pushker gera <pushkergera@pushkers-MacBook-Air.local>
2025-09-26 11:25:02 +0530

    re-set: image src

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,16 @@ const App = () => {
       <div className="container">
         <div className="row">
           <div className="col-3">
-            <UserCard name="Gustavo Herwitz" designation="UI/UX Designer" img="./public/assets/images/first-img.png"/>
+            <UserCard name="Gustavo Herwitz" designation="UI/UX Designer" img="./assets/images/first-img.png"/>
           </div>
           <div className="col-3">
-            <UserCard name="Phillip Curtis" designation="Graphic Designer" img="./public/assets/images/second-img.png" />
+            <UserCard name="Phillip Curtis" designation="Graphic Designer" img="./assets/images/second-img.png" />
           </div>
           <div className="col-3">
-            <UserCard name="Talan Torff" designation="Web Developer" img="./public/assets/images/third-img.png" />
+            <UserCard name="Talan Torff" designation="Web Developer" img="./assets/images/third-img.png" />
           </div>
           <div className="col-3">
-            <UserCard name="Abram Vaccaro" designation="App Developer" img="./public/assets/images/fourth-img.png" />
+            <UserCard name="Abram Vaccaro" designation="App Developer" img="./assets/images/fourth-img.png" />
           </div>
         </div>
       </div>
