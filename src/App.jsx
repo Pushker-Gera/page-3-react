15003dfcdb4b9589254d121107e5f8ae31bb67ae
pushker gera <pushkergera@pushkers-MacBook-Air.local>
2025-09-27 12:38:57 +0530

    recheck

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ const App = () => {
     <section>
       <div className="container">
         <div className="row">
-          {Card_Data.map((a, b) => {
-            return <div className='col-12 col-sm-6 col-md-4 col-lg-3' key={b}>
-              <UserCard name={a.name} designation={a.designation} img={a.img} />
+          {Card_Data.map((card, i) => {
+            return <div className='col-12 col-sm-6 col-md-4 col-lg-3' key={i}>
+              <UserCard name={card.name} designation={card.designation} img={card.img} />
               </div>
           })}
         </div>
