76eada226625cb40a9d67182a7c7bd54b04f11b1
pushker gera <pushkergera@pushkers-MacBook-Air.local>
2025-09-27 11:49:21 +0530

    map setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,16 @@
 import React from 'react'
 import UserCard from './components/UserCard'
+import { Card_Data } from './Card_Data'
 const App = () => {
   return (
     <section>
       <div className="container">
         <div className="row">
-          <div className="col-3">
-            <UserCard name="Gustavo Herwitz" designation="UI/UX Designer" img="./assets/images/first-img.png"/>
-          </div>
-          <div className="col-3">
-            <UserCard name="Phillip Curtis" designation="Graphic Designer" img="./assets/images/second-img.png" />
-          </div>
-          <div className="col-3">
-            <UserCard name="Talan Torff" designation="Web Developer" img="./assets/images/third-img.png" />
-          </div>
-          <div className="col-3">
-            <UserCard name="Abram Vaccaro" designation="App Developer" img="./assets/images/fourth-img.png" />
-          </div>
+          {Card_Data.map((a, b) => {
+            return <div className='col-12 col-sm-6 col-md-4 col-lg-3' key={b}>
+              <UserCard name={a.name} designation={a.designation} img={a.img} />
+              </div>
+          })}
         </div>
       </div>
     </section>
---
+++ src/Card_Data.js
@@ -0,0 +1,22 @@
+export let Card_Data = [
+    {
+        name:"Gustavo Herwitz", 
+        designation:"UI/UX Designer" ,
+        img:"./assets/images/first-img.png"
+    },
+    {
+        name: "Phillip Curtis",
+        designation: "Graphic Designer",
+        img: "./assets/images/second-img.png"
+    },
+    {
+        name: "Talan Torff",
+        designation: "Web Developer",
+        img: "./assets/images/third-img.png"
+    },
+    {
+        name: "Abram Vaccaro",
+        designation: "App Developer",
+        img: "./assets/images/fourth-img.png"
+    }
+]
